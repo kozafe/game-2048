422160671b9632ae32f59cb75aecaf123216c297
koza <koza@transfez.com>
2024-12-29 04:30:51 +0700

    fundamentals: fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,7 @@ export default async function RootLayout({
         style={{ overflow: "hidden" }}
       >
         <div className="flex items-center justify-center min-h-screen">
-          {children}
+          <Suspense fallback={<div>loading...</div>}>{children}</Suspense>
         </div>
       </body>
     </html>
